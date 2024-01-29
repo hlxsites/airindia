a18111d498960bfaca730abefd2e839ca1b12a26
Subash Selvaraj <sselvara@adobe.com>
2024-01-29 18:49:09 +0530

    addressed review comment

--- scripts/utils/initializer.js
+++ scripts/utils/initializer.js
@@ -1,6 +1,7 @@
 import {
   fetchPlaceholders, loadCSS, loadScript, toCamelCase,
 } from '../aem.js';
+import CMP_CONFIG from './constants.js';
 
 /**
  * Loads external components
@@ -12,22 +13,22 @@ import {
 export default async function loadExternalComponent(componentName, element = document.body) {
   await fetchPlaceholders();
 
-  if (window.placeholders?.default[`${toCamelCase(componentName)}Placeholder`]) {
+  if (CMP_CONFIG[`${toCamelCase(componentName)}Placeholder`]) {
     // Create a new DOMParser
     const parser = new DOMParser();
     // Parse the HTML string
-    const contentDocument = parser.parseFromString(`${window.placeholders?.default[`${toCamelCase(componentName)}Placeholder`]}`, 'text/html');
+    const contentDocument = parser.parseFromString(`${CMP_CONFIG[`${toCamelCase(componentName)}Placeholder`]}`, 'text/html');
     element.appendChild(contentDocument.body.firstChild);
   }
 
-  const scripts = window.placeholders?.default[`${toCamelCase(componentName)}Script`]?.split(',');
+  const scripts = CMP_CONFIG[`${toCamelCase(componentName)}Script`]?.split(',');
   if (scripts?.length > 0) {
     [...scripts].forEach((script) => {
       loadScript(script, { defer: true }, element);
     });
   }
 
-  const styles = window.placeholders?.default[`${toCamelCase(componentName)}Style`]?.split(',');
+  const styles = CMP_CONFIG[`${toCamelCase(componentName)}Style`]?.split(',');
   if (styles?.length > 0) {
     [...styles].forEach((style) => {
       loadCSS(style, element);
