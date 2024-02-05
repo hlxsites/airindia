bf92f30ace7ae25f93bea0ddec9326395a84c738
Subash Selvaraj <sselvara@adobe.com>
2024-02-05 10:58:11 +0530

    added mock for environment config

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const tokens = [
   'get-loy-token-key',
   'get-token-key',
   'token-key',
-  '.EnvironmentVariableServlet',
+  '.EnvironmentVariableServlet.json',
 ];
 
 let placeholders = {};
@@ -34,43 +34,43 @@ function toCamelCase(name) {
   return toClassName(name).replace(/-([a-z])/g, (g) => g[1].toUpperCase());
 }
 
-async function fetchMockTokens(key) {
+async function fetchMockPlaceholders(key) {
   try {
     // Parse and return the JSON data
     const mockData = placeholders[key];
-    console.log('Mock data:', mockData);
+    console.log('fetchMockPlaceholders: Mock data=>', mockData);
 
     return new Response(mockData, {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
-    console.error('Error fetching mock data:', error);
+    console.error('fetchMockPlaceholders: Error fetching mock data =>', error);
     // Return an error response if fetching fails
     return new Response(null, { status: 500, statusText: 'Internal Server Error' });
   }
 }
 
 async function fetchMockData(key) {
-  const path = `/mock/${key}${key.includes('json') ? '' : '.json'}`
+  const path = `/mock/${key}${key.includes('json') ? '' : '.json'}`;
   try {
     // Fetch the external JSON file
     const response = await fetch(path);
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch mock data: ${response.status} ${response.statusText}`);
+      throw new Error(`fetchMockData: Failed to fetch mock data: ${response.status} ${response.statusText}`);
     }
 
     // Parse and return the JSON data
     const mockData = await response.json();
-    console.log('Mock data:', mockData);
+    console.log('fetchMockData: Mock data =>', mockData);
 
     return new Response(JSON.stringify(mockData), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
-    console.error(`Error fetching mock data of url:${path}`, error);
+    console.error(`fetchMockData: Error fetching mock data of url:${path} =>`, error);
     // Return an error response if fetching fails
     return new Response(null, { status: 500, statusText: 'Internal Server Error' });
   }
@@ -79,15 +79,16 @@ async function fetchMockData(key) {
 // Install and activate the service worker
 self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting());
-  console.log('Service Worker installed');
+  console.log('SW-install: Service Worker installed');
 });
 
 self.addEventListener('message', (event) => {
   placeholders = event.data?.default;
+  console.log('SW-message: Placeholders received=>', placeholders);
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
+  console.log('SW-activate: Service Worker activated');
   // Perform activation tasks if needed
   event.waitUntil(self.clients.claim());
 });
@@ -98,12 +99,13 @@ self.addEventListener('fetch', async (event) => {
   // Check if the request is an API request with a different port
   if (((request.url.includes('api') && !request.url.includes('chrome-extension')) || request.url.includes('EnvironmentVariableServlet')) && hostURL.port !== '4502') {
     const paths = request.url.split('/');
-    console.log('API request and not from the port 4502:', request.url);
+    console.log('SW-fetch: API request and not from the port 4502 =>', request.url);
     if (tokens.includes(paths[paths.length - 1])) {
-      console.log('Request is for tokens:', request.url);
-      event.respondWith(fetchMockTokens(toCamelCase(paths[paths.length - 1])));
+      console.log('SW-fetch: Request is for placeholders =>', request.url);
+      event.respondWith(fetchMockPlaceholders(toCamelCase(paths[paths.length - 1])));
       return;
     }
+    console.log('SW-fetch: Request is for mock data =>', request.url);
     event.respondWith(fetchMockData(paths[paths.length - 1]));
   } else {
     // Continue with the regular fetch
