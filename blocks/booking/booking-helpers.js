49508647b2dd91438f0aa33769ba05a5ca0652a9
Subash Selvaraj <sselvara@adobe.com>
2024-04-29 14:45:59 +0530

    multicity and footer updates

--- blocks/booking/booking-helpers.js
+++ blocks/booking/booking-helpers.js
@@ -1,4 +1,5 @@
 import { fetchPlaceholders } from '../../scripts/aem.js';
+import { HOST } from '../../scripts/utils/constants.js';
 
 function initServiceWorker() {
   if ('serviceWorker' in navigator) {
@@ -23,7 +24,20 @@ function isLocalhostWithPort(port) {
          && window.location.port === port;
 }
 
+function addDomainToLinks(element) {
+  const links = element.querySelectorAll('a');
+  links.forEach((link) => {
+    // replace the origin with the HOST origin
+    if (link.href.includes(window.location.origin)) {
+      link.href = link.href.replace(window.location.origin, HOST.origin);
+    } else if (link.href.startsWith('/')) {
+      link.href = `${HOST.origin}${link.href}`;
+    }
+  });
+}
+
 export {
   initServiceWorker,
   isLocalhostWithPort,
+  addDomainToLinks,
 };
